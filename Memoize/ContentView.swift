722bdfa21d8252dd537709a9d34d6e5ef82c80b9
Dmitry Zyadik <dmitry.zyadik@gmail.com>
2023-11-19 16:29:51 +0500

    emoji foreach

--- Memoize/ContentView.swift
+++ Memoize/ContentView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
+        let emoji: [String] = ["🐦", "🐧", "🐤", "🐔"]
+
         HStack{
-            CardView(isFaceUp: true)
-            CardView()
-            CardView(isFaceUp: true)
-            CardView()
-//            Text("\(1) fileodDir \(5)")
+            ForEach(emoji.indices, id: \.self) {
+                index in CardView(content: emoji[index], isFaceUp: true)
+            }
+            //            Text("\(1) fileodDir \(5)")
         }
         .padding(20)
         .foregroundColor(.orange)
@@ -24,15 +25,16 @@ struct ContentView: View {
 }
 
 struct CardView : View {
+    var content: String
     @State var isFaceUp: Bool = false
-   
+    
     var body: some View {
         ZStack {
             let base  = RoundedRectangle(cornerRadius: 12, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
             if isFaceUp {
                 base.foregroundColor(.white)
                 base.strokeBorder( lineWidth: 2)
-                Text("🐦").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 base.fill()
             }
